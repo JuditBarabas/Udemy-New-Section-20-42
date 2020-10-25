c199d0bcd97465ec2c575be756368abaf5b367fa
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 21:13:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sumArray (arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log('Test:');
+console.log(sumArray([1, 2, 3]));
+console.log(sumArray([2, 2, 2]));
+console.log(sumArray([50, 50, 1]));
